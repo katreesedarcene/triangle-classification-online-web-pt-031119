766a7193a3e5c8571187b586317189dc4ef5029f
Kat B <katreesebarnes@gmail.com>
2019-04-14 14:31:09 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -9,7 +9,7 @@ class Triangle
   def kind 
   if ((a == b) && (a == c) && (b == c))
     return :equilateral
-  elsif ((a == b) || (a == c) || (b == c))
+  elsif ((a == b) || (b == c) || (b == c))
     return :isosceles
   else
     return :scalene
